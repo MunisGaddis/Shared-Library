221af90714cf477babbf7cefd58d443c2b208427
MunisGaddis <munishkapoorin@outlook.com>
2025-05-21 17:26:31 +0530

    Update docker_push.groovy

--- docker_push.groovy
+++ docker_push.groovy
@@ -1,16 +1,1 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', 
-                                      passwordVariable: 'dockerHubPass', 
-                                      usernameVariable: 'dockerHubUser')]) {
-        sh """
-            echo "Logging in to Docker Hub..."
-            echo \$dockerHubPass | docker login -u \$dockerHubUser --password-stdin
-            echo "Tagging image..."
-            docker tag ${Project}:${ImageTag} ${dockerhubuser}/${Project}:${ImageTag}
-            echo "Pushing image to Docker Hub..."
-            docker push ${dockerhubuser}/${Project}:${ImageTag}
-            echo "Logout from Docker Hub..."
-            docker logout
-        """
-    }
-}
+
